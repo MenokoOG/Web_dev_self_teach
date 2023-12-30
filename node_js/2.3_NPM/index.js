d9c4059997bcd86707bc2ddcfa280a107f2ebe96
Lawrence Jefferson II <93557311+MenokoOG@users.noreply.github.com>
2023-12-30 13:08:14 -0800

    first commit

---
+++ node_js/2.3_NPM/index.js
@@ -0,0 +1,11 @@
+import generateName from "sillyname"
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`)
+
+import superheroes from "superheroes"
+
+let superheroName = superheroes.random()
+
+console.log(`I am ${superheroName}!`)
+
